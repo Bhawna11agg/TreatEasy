e566386e34a988cc0cfa0dcfae1fb5030b75a475
Acash512 <aakashgupta512001@gmail.com>
2020-11-29 09:39:06 +0530

    Implemented Authentication using Firebase

--- app/src/main/java/com/example/treateasy/MainActivity.java
+++ app/src/main/java/com/example/treateasy/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.treateasy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -10,14 +18,38 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(firebaseUser!=null){
+            doAfterAuth();
+        }else{
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(false)
+                            .setAvailableProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                                    new AuthUI.IdpConfig.PhoneBuilder().build()))
+                            .build(),
+                    RC_SIGN_IN);
+        }
+    }
+
+    private void doAfterAuth() {
         // Passing each menu ID as a set of Ids because each
+        BottomNavigationView navView = findViewById(R.id.nav_view);
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
@@ -27,4 +59,29 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
     }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                 doAfterAuth();
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    finish();
+                }else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this,"Network Error",Toast.LENGTH_SHORT).show();
+                    finish();
+                }else{
+                    Toast.makeText(this,"Unknown Error Occurred",Toast.LENGTH_SHORT).show();
+                    Log.e("Auth Error", "Sign-in error: ", response.getError());
+                    finish();
+                }
+            }
+        }
+    }
 }
